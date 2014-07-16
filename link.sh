dc5b52c3bc506950be6fcae4291be2c86a2c279c
CloudNiner <andrew.fink.11@gmail.com>
2014-07-15 23:15:38 -0400

    Add global user gitignore/gitconfig

--- link.sh
+++ link.sh
@@ -4,5 +4,10 @@ THIS_DIR=`dirname $0`
 cd $THIS_DIR 
 SRC_DIR=`pwd`
 
+# TMUX
 ln -sv $SRC_DIR/tmux.conf ~/.tmux.conf
+
+# GIT
+ln -sv $SRC_DIR/gitignore ~/.gitignore
+ln -sv $SRC_DIR/gitconfig ~/.gitconfig
 
