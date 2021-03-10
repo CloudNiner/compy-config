fc7aa5a1da51d5aac66a5e7019fab2997350e258
CloudNiner <andy@finkmiller.com>
2021-03-10 10:05:36 -0500

    Add tmuxinator folder with exyn-devel config

--- link.sh
+++ link.sh
@@ -1,19 +1,20 @@
 #!/bin/bash
 
-THIS_DIR=`dirname $0`
-cd $THIS_DIR
-SRC_DIR=`pwd`
+THIS_DIR=$(dirname "$0")
+cd "$THIS_DIR" || exit
+SRC_DIR=$(pwd)
 
 # TMUX
-ln -sv $SRC_DIR/tmux.conf ~/.tmux.conf
+ln -sv "$SRC_DIR"/tmux.conf ~/.tmux.conf
+ln -sv "$SRC_DIR"/tmuxinator/configs ~/.tmuxinator
 
 # GIT
-ln -sv $SRC_DIR/gitignore ~/.gitignore
-ln -sv $SRC_DIR/gitconfig ~/.gitconfig
+ln -sv "$SRC_DIR"/gitignore ~/.gitignore
+ln -sv "$SRC_DIR"/gitconfig ~/.gitconfig
 
 # Bash Profile
-ln -sv $SRC_DIR/bash_profile ~/.bash_profile
+ln -sv "$SRC_DIR"/bash_profile ~/.bash_profile
 
 # ZSH
-ln -sv $SRC_DIR/zsh/.zshenv ~/.zshenv
-ln -sv $SRC_DIR/.hushlogin ~/.hushlogin
+ln -sv "$SRC_DIR"/zsh/.zshenv ~/.zshenv
+ln -sv "$SRC_DIR"/.hushlogin ~/.hushlogin
