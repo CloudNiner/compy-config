5a3fa6dc836588fddd8c013d7abe9e77344196aa
CloudNiner <andy@finkmiller.com>
2026-02-08 14:33:18 -0500

    Add basic fish config using starship

--- link.sh
+++ link.sh
@@ -4,12 +4,21 @@ THIS_DIR=$(dirname "$0")
 cd "$THIS_DIR" || exit
 SRC_DIR=$(pwd)
 
+mkdir -p ~/.config
+
 # TMUX
 ln -sv "$SRC_DIR"/tmux.conf ~/.tmux.conf
 
 # GIT
 ln -sv "$SRC_DIR"/gitignore ~/.gitignore
 ln -sv "$SRC_DIR"/gitconfig ~/.gitconfig
+
+# Fish
+mkdir -p ~/.config/fish
+ln -sv "$SRC_DIR"/fish/config.fish ~/.config/fish/config.fish
+
+# Starship
+ln -sv "$SRC_DIR"/starship.toml ~/.config/starship.toml
 
 # psql
 ln -sv "$SRC_DIR"/psqlrc ~/.psqlrc
